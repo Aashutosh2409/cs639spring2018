1c3f74196f67921483e2e9b075e566a860c852e4
Aashutosh2409 <as03681n@pace.edu>
2018-02-09 14:57:46 -0500

    First Commit after adding basic functionality

--- CS639SpringHW1/app/src/main/java/com/pace/cs639spring/hw1/AnimalDisplayFragment.java
+++ CS639SpringHW1/app/src/main/java/com/pace/cs639spring/hw1/AnimalDisplayFragment.java
@@ -1,22 +1,120 @@
 package com.pace.cs639spring.hw1;
 
-import android.app.Fragment;
+import android.graphics.PorterDuff;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by kachi on 1/31/18.
  */
 
-public class AnimalDisplayFragment extends Fragment {
+public class AnimalDisplayFragment extends Fragment implements View.OnClickListener {
+    //instance of View and Text part
+    private ImageView biView,ciView,diView;
+    private TextView biText, ciText, diText;
+    //pointer to store the event action number.
+    int pointer = -1;
+
 
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.animal_display, container, false);
+      //2 mothods
+        define(view);
+        setView();
         return view;
+
+    }
+
+    private void define(View view) {
+        biView = view.findViewById(R.id.iBird);
+        ciView = view.findViewById(R.id.iCat);
+        diView = view.findViewById(R.id.iDog);
+
+        diText=view.findViewById(R.id.tDog);
+        ciText=view.findViewById(R.id.tCat);
+        biText=view.findViewById(R.id.tBird);
+    }
+
+    private void setView() {
+        biView.setOnClickListener(this);
+        ciView.setOnClickListener(this);
+        diView.setOnClickListener(this);
+
+        biText.setOnClickListener(this);
+        ciText.setOnClickListener(this);
+        ciText.setOnClickListener(this);
+
+    }
+
+    public void colorM(int c) {
+        if (pointer == -1) {
+            Toast.makeText(getActivity(), getString(R.string.b), Toast.LENGTH_SHORT).show();
+        } else if (pointer == 1) {
+            biView.setColorFilter(c, PorterDuff.Mode.SRC_IN);
+        } else if (pointer == 2) {
+            ciView.setColorFilter(c, PorterDuff.Mode.SRC_IN);
+        } else if (pointer == 3) {
+            diView.setColorFilter(c, PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+//selecting on click event and assigning pointer.
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iBird:
+                pointer = 1;
+                break;
+            case R.id.iCat:
+                pointer = 2;
+                break;
+            case R.id.iDog:
+                pointer = 3;
+                break;
+        }
+        information(); //call of method, to show respective messages.
+    }
+
+    private void information() {
+        if (pointer == 1) {
+            if (biText.getVisibility() == View.GONE) {
+                biText.setVisibility(View.VISIBLE);
+            } else {
+                pointer = -1;
+                biText.setVisibility(View.GONE);
+            }
+            ciText.setVisibility(View.GONE);
+            diText.setVisibility(View.GONE);
+
+        } else if (pointer == 2) {
+            biText.setVisibility(View.GONE);
+            if (ciText.getVisibility() == View.GONE) {
+                ciText.setVisibility(View.VISIBLE);
+            } else {
+                pointer = -1;
+                ciText.setVisibility(View.GONE);
+            }
+            diText.setVisibility(View.GONE);
+        } else if (pointer == 3) {
+            biText.setVisibility(View.GONE);
+            ciText.setVisibility(View.GONE);
+            if (diText.getVisibility() == View.GONE) {
+                diText.setVisibility(View.VISIBLE);
+            } else {
+                pointer = -1;
+                diText.setVisibility(View.GONE);
+            }
+
+        }
     }
 }
--- CS639SpringHW1/app/src/main/java/com/pace/cs639spring/hw1/MainActivity.java
+++ CS639SpringHW1/app/src/main/java/com/pace/cs639spring/hw1/MainActivity.java
@@ -1,13 +1,70 @@
 package com.pace.cs639spring.hw1;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private AnimalDisplayFragment animalDisplayFragment;
+    private View vGreen, vBlue, vOrange, vPurple, vRed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        setReference();
+        setEvents();
+
+        animalDisplayFragment = new AnimalDisplayFragment();
+        addFrag(R.id.box,animalDisplayFragment,getString(R.string.fragment_animal));
+        Toast.makeText(getApplicationContext(),getString(R.string.tb), Toast.LENGTH_SHORT).show();
+    }
+
+    private void setReference() {
+        vPurple = findViewById(R.id.purple1);
+        vOrange = findViewById(R.id.orange1);
+        vRed = findViewById(R.id.red1);
+        vGreen = findViewById(R.id.green1);
+        vBlue = findViewById(R.id.blue1);
+
+    }
+
+    private void setEvents() {
+        vBlue.setOnClickListener(this);
+        vGreen.setOnClickListener(this);
+        vRed.setOnClickListener(this);
+        vOrange.setOnClickListener(this);
+        vPurple.setOnClickListener(this);
+    }
+
+    protected void addFrag(int containerViewId,Fragment fragment,String fTag)
+    {
+    getSupportFragmentManager().beginTransaction().add(containerViewId, fragment, fTag).disallowAddToBackStack().commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.purple1:
+                animalDisplayFragment.colorM(ContextCompat.getColor(this, android.R.color.holo_purple));
+                break;
+            case R.id.green1:
+                animalDisplayFragment.colorM(ContextCompat.getColor(this, android.R.color.holo_green_dark));
+                break;
+            case R.id.orange1:
+                animalDisplayFragment.colorM(ContextCompat.getColor(this, android.R.color.holo_orange_dark));
+                break;
+            case R.id.blue1:
+                animalDisplayFragment.colorM(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
+                break;
+            case R.id.red1:
+                animalDisplayFragment.colorM(ContextCompat.getColor(this, android.R.color.holo_red_dark));
+                break;
+        }
     }
 }
